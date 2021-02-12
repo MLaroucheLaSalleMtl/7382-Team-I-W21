7e17e7fb06ce28613cf8e87ac8ce09bc961321b8
Chengyi Liao <lcy804585626@yahoo.ca>
2021-02-11 21:35:12 -0500

    Basic Level System Logic and UI

--- Assets/Scripts/LevelSystem.cs
+++ Assets/Scripts/LevelSystem.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelSystem
 {
+    public event EventHandler OnExperienceChanged;
+    public event EventHandler OnLevelChanged;
     private int level;
     private int experience;
     private int experienceToNextLevel;
@@ -17,7 +20,7 @@ public class LevelSystem
     }
 
     //basic level system logic
-    private void LevelUp(int amount)
+    public void LevelUp(int amount)
     {
         experience += amount;
         if (experience >= experienceToNextLevel)
@@ -25,6 +28,18 @@ public class LevelSystem
             //gain enough exp to level up
             level++;
             experience -= experienceToNextLevel;
+            if (OnLevelChanged != null) OnLevelChanged(this, EventArgs.Empty);
         }
+        if (OnExperienceChanged != null) OnExperienceChanged(this, EventArgs.Empty);
+    }
+
+    public int GetLevelNumber()
+    {
+        return level;
+    }
+
+    public float GetExperienceNormalized()
+    {
+        return experience / experienceToNextLevel;
     }
 }
---
+++ Assets/Scripts/LevelWindow.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelWindow : MonoBehaviour
+{
+    [SerializeField] private Button addExperienceButton;
+
+    private Text levelText;
+    private Image experienceBarImage;
+    private LevelSystem levelSystem;
+
+    private void Start()
+    {
+        Button addExperienceBtn = addExperienceButton.GetComponent<Button>();
+        addExperienceBtn.onClick.AddListener(levelSystem.LevelUp(50));
+    }
+    private void Awake()
+    {
+        levelText = transform.Find("LevelTxt").GetComponent<Text>();
+        experienceBarImage = transform.Find("ExperienceBar").Find("Bar").GetComponent<Image>();
+
+        transform.Find("ExperienceAddBtn50").GetComponent<Button>().onClick = levelSystem.LevelUp(50);
+    }
+
+    private void SetExperienceBarSize(float experienceNormalized)
+    {
+        experienceBarImage.fillAmount = experienceNormalized;
+    }
+
+    private void SetLevelNumber(int levelNumber)
+    {
+        levelText.text = "LEVEL\n" + (levelNumber + 1);
+    }
+
+    private void SetLevelSystem(LevelSystem levelSystem)
+    {
+        //Set the LevelSystem object
+        this.levelSystem = levelSystem;
+
+        //Update the starting values
+        SetLevelNumber(levelSystem.GetLevelNumber());
+        SetExperienceBarSize(levelSystem.GetExperienceNormalized());
+
+        //Subscribe to the changed events
+        levelSystem.OnExperienceChanged += LevelSystem_OnExperienceChanged;
+        levelSystem.OnLevelChanged += LevelSystem_OnLevelChanged;
+    }
+
+    private void LevelSystem_OnLevelChanged(object sender, System.EventArgs e)
+    {
+        //Level changed, update next
+        SetLevelNumber(levelSystem.GetLevelNumber());
+    }
+
+    private void LevelSystem_OnExperienceChanged(object sender, System.EventArgs e)
+    {
+        //Experience changed, update bar size
+        SetExperienceBarSize(levelSystem.GetExperienceNormalized());
+    }
+}
