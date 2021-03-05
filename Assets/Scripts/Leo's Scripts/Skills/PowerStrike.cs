ac3971176d960ec5ba82c3cd4cdef932eb346573
Chengyi Liao <lcy804585626@yahoo.ca>
2021-03-04 21:25:13 -0500

    Animation added

--- Assets/Scripts/Leo's Scripts/Skills/PowerStrike.cs
+++ Assets/Scripts/Leo's Scripts/Skills/PowerStrike.cs
@@ -6,15 +6,21 @@ namespace Skills
 {
     public class PowerStrike : MonoBehaviour
     {
+        private Animator anim;
         private void Start()
         {
-
+            anim = GetComponent<Animator>();
         }
         private void Update()
+        {
+            CastPowerStrike();
+        }
+        void CastPowerStrike()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 GetComponent<ZombieHealth>().Hurt(zombieHurt: 2);
+                anim.SetBool("isAttacking", true);
             }
         }
     }
